2f99d93f3bcfe176ceb6940d664c0b3adb3c641a
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-03-06 20:06:38 +0100

    refactor(admin): improve layout and formatting of the admin article management interface

--- views/templates/admin.php
+++ views/templates/admin.php
@@ -1,21 +1,42 @@
-<?php 
-    /** 
-     * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun. 
-     * Et un formulaire pour ajouter un article. 
-     */
+<?php
+
+/**
+ * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun.
+ * Et un formulaire pour ajouter un article.
+ */
 ?>
 
-<h2>Edition des articles</h2>
+<div class="admin-header">
+    <h2>Administration des articles</h2>
+
+    <nav class="admin-nav">
+        <a href="index.php?action=admin" class="nav-link active">
+            Édition
+        </a>
+        <a href="index.php?action=showArticleList" class="nav-link">
+            Monitoring
+        </a>
+    </nav>
+</div>
 
 <div class="adminArticle">
+
     <?php foreach ($articles as $article) { ?>
+
         <div class="articleLine">
+
             <div class="title"><?= $article->getTitle() ?></div>
+
             <div class="content"><?= $article->getContent(200) ?></div>
+
             <div><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
-            <div><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a></div>
+
+            <div><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?>>Supprimer</a></div>
+
         </div>
+
     <?php } ?>
+
 </div>
 
 <a class="submit" href="index.php?action=showUpdateArticleForm">Ajouter un article</a>
